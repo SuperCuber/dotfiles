a498d9d58dcafc641b72646782bfbec7799dc6b2
SuperCuber <amit.gold00@gmail.com>
2021-12-24 22:15:55 +0200

    Tweak colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,4 +4,12 @@ require("supercuber.keymaps")
 require("supercuber.language")
 
 vim.cmd [[au FileType fugitive nmap <buffer> <tab> =]]
+require('kanagawa').setup({
+    commentStyle = "NONE",
+    keywordStyle = "bold",
+    statementStyle = "bold",
+    variablebuiltinStyle = "bold",
+    specialReturn = false,       -- special highlight for the return keyword
+    specialException = false,    -- special highlight for exception handling keywords
+})
 vim.cmd [[colorscheme kanagawa]]
