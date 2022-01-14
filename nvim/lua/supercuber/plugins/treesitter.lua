f9f533296d594559236c3265e4a7dce2ef47bc2e
SuperCuber <amit.gold00@gmail.com>
2022-01-15 00:45:38 +0200

    Remove treesitter ensure installed because it crashes the VPS

--- nvim/lua/supercuber/plugins/treesitter.lua
+++ nvim/lua/supercuber/plugins/treesitter.lua
@@ -1,10 +1,4 @@
 require'nvim-treesitter.configs'.setup {
-  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = "maintained",
-
-  -- Install languages synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
   highlight = {
     enable = true,
   },
