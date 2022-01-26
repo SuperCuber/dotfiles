0074cbf00880048a3256df52c7d5502243e48cc5
SuperCuber <amit.gold00@gmail.com>
2022-01-26 23:38:06 +0200

    Fix bad syntax in lualine

--- nvim/lua/supercuber/plugins/lualine.lua
+++ nvim/lua/supercuber/plugins/lualine.lua
@@ -108,7 +108,9 @@ require'lualine'.setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+      {'filename', path = 1}
+    },
     lualine_x = {},
     lualine_y = {},
     lualine_z = {}
