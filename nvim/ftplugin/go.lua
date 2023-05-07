8a887ff410cec55ad6c44e3c2ee9fdec7c610553
amit.gold <amit.gold@appsflyer.com>
2023-05-07 10:06:08 +0300

    go tabs

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<Leader>m", "<Cmd>Make lint<Enter>", {buffer=true})
 vim.keymap.set("n", "<Leader>t", "<Cmd>Make test<Enter>", {buffer=true})
 vim.opt.colorcolumn = "140"
+vim.opt.expandtab = false
