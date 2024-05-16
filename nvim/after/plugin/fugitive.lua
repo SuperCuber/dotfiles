da173224f7e8aca44a1c49c4b09ffb5d71b93926
amit.gold <amit.gold@appsflyer.com>
2024-05-16 16:25:57 +0300

    Fix :GBrowse

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -8,3 +8,11 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 vim.keymap.set("n", "<Leader>g", vim.cmd.Git)
 vim.g.fugitive_gitlab_domains = { "https://gitlab.appsflyer.com" }
+vim.api.nvim_create_user_command(
+  'Browse',
+  function (opts)
+    -- windows->start, mac->open, linux->xdg-open
+    vim.fn.system { '{{#if (eq dotter.os "windows")}}start{{else}}{{#if (is_executable "open")}}open{{else}}xdg-open{{/if}}{{/if}}', opts.fargs[1] }
+  end,
+  { nargs = 1 }
+)
