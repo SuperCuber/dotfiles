e859dcc05f5c6ef4deab068f7e16f15b7c7147f7
amit.gold <amit.gold@appsflyer.com>
2024-07-14 22:53:06 +0300

    remove usless neorg stuff

--- nvim/lua/supercuber/plugins/neorg.lua
+++ nvim/lua/supercuber/plugins/neorg.lua
@@ -1,54 +1,42 @@
-local neorg_doesnt_work = "{{neorg_doesnt_work}}"
-
-if neorg_doesnt_work ~= "true" then
-    local function config()
-        require("neorg").setup({
-            load = {
-                ["core.defaults"] = {},
-                ["core.dirman"] = {
-                    config = {
-                        workspaces = {
-                            main = "~/neorg",
-                        },
-                        default_workspace = "main",
-                        index = "index.norg",
+local function config()
+    require("neorg").setup({
+        load = {
+            ["core.defaults"] = {},
+            ["core.dirman"] = {
+                config = {
+                    workspaces = {
+                        main = "~/neorg",
+                    },
+                    default_workspace = "main",
+                    index = "index.norg",
+                },
+            },
+            ["core.journal"] = {
+                config = {
+                    strategy = "flat",
+                    workspace = "main",
+                },
+            },
+            ["core.concealer"] = {
+                config = {
+                    folds = false,
+                    icons = {
+                        todo = false,
                     },
                 },
-                ["core.journal"] = {
-                    config = {
-                        strategy = "flat",
-                        workspace = "main",
-                    },
-                },
-                ["core.concealer"] = {
-                    config = {
-                        folds = false,
-                        icons = {
-                            todo = false,
-                        },
-                    },
-                },
-            }
-        })
+            },
+        }
+    })
 
-        vim.keymap.set("n", ",nt", function()
-            vim.cmd.tabnew()
-            vim.cmd [[Neorg journal today]]
-            vim.cmd.tcd("~/neorg")
-        end)
-    end
-
-    return {
-        "nvim-neorg/neorg",
-        rocks = { "lua-utils.nvim", "nvim-nio", "nui.nvim", "plenary.nvim", "pathlib.nvim" },
-        config = config,
-    }
-else
     vim.keymap.set("n", ",nt", function()
         vim.cmd.tabnew()
-        local today = os.date("%Y-%m-%d")
-        vim.cmd([[e ~/neorg/journal/]] .. today .. [[.norg]])
+        vim.cmd [[Neorg journal today]]
+        vim.cmd.tcd("~/neorg")
     end)
-    return {}
 end
 
+return {
+    "nvim-neorg/neorg",
+    rocks = { "lua-utils.nvim", "nvim-nio", "nui.nvim", "plenary.nvim", "pathlib.nvim" },
+    config = config,
+}
