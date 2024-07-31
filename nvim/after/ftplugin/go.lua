37b69b4701027fc78c7925f136278064a3b7b337
amit.gold <amit.gold@appsflyer.com>
2024-07-31 15:47:44 +0300

    Add harpoon and fmt files

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<LocalLeader>m", "<Cmd>Make lint<Enter>", {buffer=true})
-vim.keymap.set("n", "<LocalLeader>t", "<Cmd>Make test<Enter>", {buffer=true})
+vim.keymap.set("n", "<LocalLeader>m", "<Cmd>Make lint<Enter>", { buffer = true })
+vim.keymap.set("n", "<LocalLeader>t", "<Cmd>Make test<Enter>", { buffer = true })
 vim.opt.colorcolumn = "140"
 vim.opt.expandtab = false
