d73e82f44b552be4c72e26d59c9d9c4bab93e063
amit.gold <amit.gold@appsflyer.com>
2025-07-19 19:17:14 +0300

    fuck grt

--- nvim/lua/supercuber/plugins/lsp.lua
+++ nvim/lua/supercuber/plugins/lsp.lua
@@ -30,7 +30,7 @@ local function config()
         { desc = "vim.lsp.buf.document_highlight()" })
 
     -- default nvim keybinds
-    for _, key in ipairs({ "grn", "gra", "grr", "n", "gri", "gO" }) do
+    for _, key in ipairs({ "grn", "gra", "grr", "n", "gri", "gO", "grt" }) do
         for _, mode in ipairs({ "n", "x" }) do
             if vim.fn.mapcheck(key, mode) ~= "" then
                 vim.keymap.del(mode, key)
